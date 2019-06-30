e52e8853d7fae288e3681bcb71f3ad8a3a61a9b5
sbash64 <sbash64@gmail.com>
2019-06-30 13:47:09 -0500

    pass

--- phase-vocoder/tests/OverlapExtractTests.cpp
+++ phase-vocoder/tests/OverlapExtractTests.cpp
@@ -3,16 +3,20 @@
 template<typename T>
 class OverlapExtract {
 	std::vector<T> signal;
+	int head;
+	int hop;
 	int N;
 public:
-	OverlapExtract(int N, int ) : N{ N } {}
+	OverlapExtract(int N, int hop) : N{ N }, hop{ hop }, head{ 0 } {}
 
 	void add(std::vector<T> x) {
 		signal = x;
 	}
 
 	std::vector<T> next() {
-		return { signal.begin(), signal.begin() + N };
+		auto head_ = signal.begin() + head;
+		head += hop;
+		return { head_, head_ + N };
 	}
 };
 
