a0c5b1f9c398433ba0247870641d1c54f4ecd621
sbash64 <sbash64@gmail.com>
2019-07-07 19:05:54 -0500

    add test

--- phase-vocoder/include/phase-vocoder/InterpolateFrames.h
+++ phase-vocoder/include/phase-vocoder/InterpolateFrames.h
@@ -10,6 +10,9 @@ class InterpolateFrames {
 	using complex_type = std::complex<T>;
 	std::vector<complex_type> previous;
 	std::vector<complex_type> current;
+	std::vector<T> accumulatedPhase;
+	std::vector<T> phaseAdvance;
+	std::vector<T> resampledMagnitude;
 	int numerator;
 	int P;
 	int Q;
@@ -17,6 +20,9 @@ public:
 	InterpolateFrames(int P, int Q, int N) :
 		previous(N),
 		current(N),
+		accumulatedPhase(N),
+		phaseAdvance(N),
+		resampledMagnitude(N),
 		numerator{ 0 },
 		P{ P },
 		Q{ Q }
@@ -35,17 +41,42 @@ public:
 			previous.begin(),
 			previous.end(),
 			current.begin(),
-			x.begin(),
+			phaseAdvance.begin(),
+			[&](complex_type a, complex_type b) {
+				return std::arg(b) - std::arg(a);
+			}
+		);
+		std::transform(
+			previous.begin(),
+			previous.end(),
+			current.begin(),
+			resampledMagnitude.begin(),
 			[&](complex_type a, complex_type b) {
 				T denominator = Q;
 				auto ratio = numerator / denominator;
-				auto magnitude = std::abs(a) * (1 - ratio) + std::abs(b) * ratio;
-				auto phase = std::arg(b) - std::arg(a);
+				return std::abs(a) * (1 - ratio) + std::abs(b) * ratio;
+			}
+		);
+		std::transform(
+			phaseAdvance.begin(),
+			phaseAdvance.end(),
+			accumulatedPhase.begin(),
+			accumulatedPhase.begin(),
+			std::plus<double>{}
+		);
+		std::transform(
+			resampledMagnitude.begin(),
+			resampledMagnitude.end(),
+			accumulatedPhase.begin(),
+			x.begin(),
+			[&](T magnitude, T phase) {
 				return std::polar(magnitude, phase);
 			}
 		);
-		if (numerator == Q)
+		if (numerator == Q) {
 			numerator -= Q;
+			previous = current;
+		}
 		else
 			numerator += P;
 	}
--- phase-vocoder/tests/InterpolateFramesTests.cpp
+++ phase-vocoder/tests/InterpolateFramesTests.cpp
@@ -38,20 +38,69 @@ namespace {
 			interpolate.add(x);
 		}
 
-		void assertNextEquals(std::vector<std::complex<double>> expected, double tolerance) {
+		void addConsume(std::vector<std::complex<double>> x) {
+			add(x);
+			while (hasNext())
+				next();
+		}
+
+		std::vector<std::complex<double>> next() {
 			std::vector<std::complex<double>> out(N);
 			interpolate.next(out);
-			assertEqual(std::move(expected), std::move(out), tolerance);
+			return out;
+		}
+
+		void assertNextEquals(std::vector<std::complex<double>> expected, double tolerance) {
+			assertEqual(std::move(expected), next(), tolerance);
+		}
+
+		double magnitude(std::complex<double> x) {
+			return std::abs(x);
+		}
+
+		double phase(std::complex<double> x) {
+			return std::arg(x);
+		}
+
+		std::complex<double> toComplex(double magnitude, double radians) {
+			return std::polar(magnitude, radians);
 		}
 	};
 
-	TEST_F(InterpolateFramesTests, tbd) {
+	TEST_F(InterpolateFramesTests, interpolatesComplexMagnitudesAndAdvancesPhase) {
 		using namespace std::complex_literals;
 		assertInterpolatedFrames(
 			{1.0 + 2i, 3.0 + 4i, 5.0 + 6i},
 			{
 				{ 0.5 + 1i, 1.5 + 2i, 2.5 + 3i},
-				{ 1.0 + 2i, 3.0 + 4i, 5.0 + 6i}
+				{
+					toComplex(magnitude(1.0 + 2i), 2 * phase(1.0 + 2i)),
+					toComplex(magnitude(3.0 + 4i), 2 * phase(3.0 + 4i)),
+					toComplex(magnitude(5.0 + 6i), 2 * phase(5.0 + 6i))
+				}
+			},
+			1e-15
+		);
+	}
+
+	TEST_F(InterpolateFramesTests, interpolatesComplexMagnitudesAndAdvancesPhase2) {
+		using namespace std::complex_literals;
+		addConsume(
+			{ 1.0 + 2i, 3.0 + 4i, 5.0 + 6i }
+		);
+		assertInterpolatedFrames(
+			{ 7.0 + 8i, 9.0 + 10i, 11.0 + 12i },
+			{
+				{
+					toComplex((magnitude(1.0 + 2i) + magnitude(7.0 + 8i)) / 2, phase(7.0 + 8i) + phase(1.0 + 2i)),
+					toComplex((magnitude(3.0 + 4i) + magnitude(9.0 + 10i)) / 2, phase(9.0 + 10i) + phase(3.0 + 4i)),
+					toComplex((magnitude(5.0 + 6i) + magnitude(11.0 + 12i)) / 2, phase(11.0 + 12i) + phase(5.0 + 6i)),
+				},
+				{
+					toComplex(magnitude(7.0 + 8i), 2 * phase(7.0 + 8i)),
+					toComplex(magnitude(9.0 + 10i), 2 * phase(9.0 + 10i)),
+					toComplex(magnitude(11.0 + 12i), 2 * phase(11.0 + 12i))
+				}
 			},
 			1e-15
 		);
