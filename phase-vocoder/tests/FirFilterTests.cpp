1af4f86e651f1f0777bec06303e69a0d816a3ff7
sbash64 <sbash64@gmail.com>
2019-07-21 13:20:05 -0500

    refactor

--- phase-vocoder/tests/FirFilterTests.cpp
+++ phase-vocoder/tests/FirFilterTests.cpp
@@ -17,24 +17,28 @@ public:
 namespace {
 	class FirFilterTests : public ::testing::Test {
 	protected:
+		std::vector<double> b{ 0 };
+
 		void assertFilteredOutput(
-			std::vector<double> b, 
 			std::vector<double> x, 
 			const std::vector<double>& y
 		) {
-			FirFilter<double> filter{ std::move(b) };
+			FirFilter<double> filter{ b };
 			filter.filter(x);
 			assertEqual(y, x);
+		}
+
+		void setCoefficients(std::vector<double> b_) {
+			b = std::move(b_);
 		}
 	};
 
 	TEST_F(FirFilterTests, emptyInputYieldsEmpty) {
-		std::vector<double> b = { 1 };
-		assertFilteredOutput(b, {}, {});
+		assertFilteredOutput({}, {});
 	}
 
 	TEST_F(FirFilterTests, identityYieldsSame) {
-		std::vector<double> b = { 1 };
-		assertFilteredOutput(b, {2, 3, 4}, {2, 3, 4});
+		setCoefficients({ 1 });
+		assertFilteredOutput({2, 3, 4}, {2, 3, 4});
 	}
 }
