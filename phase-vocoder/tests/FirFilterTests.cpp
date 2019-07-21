371ea48cc68063fb88b400e9a4b425cbf474b5f8
sbash64 <sbash64@gmail.com>
2019-07-21 13:10:09 -0500

    add test

---
+++ phase-vocoder/tests/FirFilterTests.cpp
@@ -0,0 +1,36 @@
+#include <gsl/gsl>
+#include <vector>
+
+template<typename T>
+class FirFilter {
+public:
+	FirFilter(std::vector<T> b) {}
+
+	void filter(gsl::span<T> x) {
+		x;
+	}
+};
+
+#include "assert-utility.h"
+#include <gtest/gtest.h>
+
+namespace {
+	class FirFilterTests : public ::testing::Test {
+	protected:
+		void assertFilteredOutput(
+			std::vector<double> b, 
+			const std::vector<double>& x, 
+			const std::vector<double>& y
+		) {
+			FirFilter<double> filter{ std::move(b) };
+			std::vector<double> output{ x };
+			filter.filter(output);
+			assertEqual(y, output);
+		}
+	};
+
+	TEST_F(FirFilterTests, emptyInputYieldsEmpty) {
+		std::vector<double> b = { 1 };
+		assertFilteredOutput(b, {}, {});
+	}
+}
