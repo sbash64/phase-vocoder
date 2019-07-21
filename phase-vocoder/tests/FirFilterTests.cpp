79902c965c8000ade158576b3d4e656e7209e956
sbash64 <sbash64@gmail.com>
2019-07-21 13:44:55 -0500

    add test

--- phase-vocoder/tests/FirFilterTests.cpp
+++ phase-vocoder/tests/FirFilterTests.cpp
@@ -8,8 +8,12 @@ public:
 	FirFilter(std::vector<T> b) : b{ std::move(b) } {}
 
 	void filter(gsl::span<T> x) {
-		for (auto &x_ : x)
-			x_ *= b.front();
+		for (auto i = x.size() - 1; i >= 0; --i) {
+			T accumulate{ 0 };
+			for (int j = 0; j <= std::min(gsl::narrow<size_t>(i), b.size() - 1); ++j)
+				accumulate += b[j] * x[i - j];
+			x[i] = accumulate;
+		}
 	}
 };
 
@@ -47,5 +51,10 @@ namespace {
 	TEST_F(FirFilterTests, doubleYieldsDouble) {
 		setCoefficients({ 2 });
 		assertFilteredOutput({ 3, 4, 5 }, { 6, 8, 10 });
+	}
+
+	TEST_F(FirFilterTests, simpleConvolution) {
+		setCoefficients({ 1, 2, 3 });
+		assertFilteredOutput({ 4, 5, 6 }, { 1*4, 1*5 + 4*2, 1*6 + 2*5 + 3*4 });
 	}
 }
