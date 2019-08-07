5ba63700530421990b0ebf1bab4f84929f446294
Seth Bashford <sbash64@gmail.com>
2019-08-07 12:10:57 -0500

    factor out

--- phase-vocoder/tests/FirFilterTests.cpp
+++ phase-vocoder/tests/FirFilterTests.cpp
@@ -1,29 +1,5 @@
-#include <gsl/gsl>
-#include <vector>
-#include <algorithm>
-
-template<typename T>
-class FirFilter {
-	std::vector<T> b;
-	std::vector<T> delayLine;
-public:
-	FirFilter(std::vector<T> b) : 
-		b{ std::move(b) },
-		delayLine(this->b.size())
-	{}
-	void filter(gsl::span<T> x) {
-		for (auto &x_ : x) {
-			delayLine.front() = x_;
-			T accumulate{ 0 };
-			for (typename std::vector<T>::size_type j{ 0 }; j < b.size(); ++j)
-				accumulate += b[j] * delayLine[j];
-			x_ = accumulate;
-			std::rotate(delayLine.rbegin(), delayLine.rbegin() + 1, delayLine.rend());
-		}
-	}
-};
-
 #include "assert-utility.h"
+#include <phase-vocoder/FirFilter.h>
 #include <gtest/gtest.h>
 
 namespace {
