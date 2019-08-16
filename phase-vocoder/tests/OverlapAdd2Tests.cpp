37e091d46e3e616448fbc8ee36fe895f98c211e7
Seth Bashford <sbash64@gmail.com>
2019-08-16 10:48:28 -0500

    change meaning of test

--- phase-vocoder/tests/OverlapAdd2Tests.cpp
+++ phase-vocoder/tests/OverlapAdd2Tests.cpp
@@ -4,16 +4,25 @@
 namespace phase_vocoder {
 template<typename T>
 class OverlapAdd2 {
-    std::vector<T> buffer_;
+    std::vector<T> overlap_;
+    std::vector<T> next_;
+    int hop;
 public:
-    explicit OverlapAdd2(int N) : buffer_(N) {}
-    
-    void add(std::vector<T> x) {
-        std::copy(x.begin(), x.end(), buffer_.begin());
+    explicit OverlapAdd2(int N, int hop) : overlap_(N), next_(N), hop{hop} {}
+
+    void add(gsl::span<const T> x) {
+        std::copy(x.begin(), x.end(), next_.begin());
+        std::transform(
+            overlap_.begin() + hop,
+            overlap_.end(),
+            x.begin(),
+            overlap_.begin() + hop,
+            std::plus<>{}
+        );
     }
 
     void overlap(gsl::span<T> y) {
-        std::copy(buffer_.begin(), buffer_.end(), y.begin());
+        std::copy(overlap_.begin(), overlap_.end(), y.begin());
     }
 };
 }
@@ -27,7 +36,7 @@ constexpr auto hop = 2;
 class OverlapAdd2Tests : public ::testing::Test {
 protected:
     void assertOverlap(std::vector<double> x, const std::vector<double> &y) {
-        phase_vocoder::OverlapAdd2<double> overlapAdd(N);
+        phase_vocoder::OverlapAdd2<double> overlapAdd(N, hop);
         overlapAdd.add(std::move(x));
         std::vector<double> overlap_(N);
         overlapAdd.overlap(overlap_);
@@ -35,7 +44,7 @@ protected:
     }
 };
 
-TEST_F(OverlapAdd2Tests, tbd) {
-    assertOverlap({ 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 });
+TEST_F(OverlapAdd2Tests, firstBlockAddedToZeros) {
+    assertOverlap({ 1, 2, 3, 4, 5 }, { 0, 0, 1, 2, 3 });
 }
 }
