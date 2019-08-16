3b1421d5bed97d637955c03bba8c2bc4d61ddd59
Seth Bashford <sbash64@gmail.com>
2019-08-16 11:04:38 -0500

    refactor

--- phase-vocoder/tests/OverlapAdd2Tests.cpp
+++ phase-vocoder/tests/OverlapAdd2Tests.cpp
@@ -39,20 +39,24 @@ constexpr auto N = 5;
 constexpr auto hop = 2;
 class OverlapAdd2Tests : public ::testing::Test {
     phase_vocoder::OverlapAdd2<double> overlapAdd{N, hop};
+    std::vector<double> overlap_;
 protected:
+    OverlapAdd2Tests() : overlap_(N) {}
+    
     void assertOverlap(
         const std::vector<double> &x,
         const std::vector<double> &y
     ) {
-        overlapAdd.add(x);
-        std::vector<double> overlap_(N);
-        overlapAdd.next(overlap_);
+        consumeAdd_(x);
         assertEqual(y, overlap_);
     }
 
     void consumeAdd(const std::vector<double> &x) {
+        consumeAdd_(x);
+    }
+
+    void consumeAdd_(const std::vector<double> &x) {
         overlapAdd.add(x);
-        std::vector<double> overlap_(N);
         overlapAdd.next(overlap_);
     }
 };
@@ -61,7 +65,7 @@ TEST_F(OverlapAdd2Tests, firstBlockAddedToZeros) {
     assertOverlap({ 1, 2, 3, 4, 5 }, { 0, 0, 1, 2, 3 });
 }
 
-TEST_F(OverlapAdd2Tests, secondBlockOverlapAddedToFirst) {
+TEST_F(OverlapAdd2Tests, nextBlockOverlapAddedToPrevious) {
     consumeAdd({ 1, 2, 3, 4, 5 });
     assertOverlap({ 6, 7, 8, 9, 10 }, { 1, 2, 3+6, 4+7, 5+8 });
 }
