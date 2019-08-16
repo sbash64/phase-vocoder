a6be0722cb6d690244e9fa779bea4cf4ec713619
Seth Bashford <sbash64@gmail.com>
2019-08-16 15:48:44 -0500

    Expand 8 16 19 (#7)

---
+++ phase-vocoder/include/phase-vocoder/Expand.h
@@ -0,0 +1,22 @@
+#ifndef PHASEVOCODER_EXPAND_H
+#define PHASEVOCODER_EXPAND_H
+
+#include <gsl/gsl>
+#include <vector>
+
+namespace phase_vocoder {
+class Expand {
+    int P;
+public:
+    explicit Expand(int P) : P{P} {}
+
+    template<typename T>
+    void expand(gsl::span<const T> x, gsl::span<T> y) {
+        std::fill(y.begin(), y.end(), 0);
+        for (typename gsl::span<T>::index_type i{0}; i < x.size(); ++i)
+            y.at(i*P) = x.at(i);
+    }
+};
+}
+
+#endif
