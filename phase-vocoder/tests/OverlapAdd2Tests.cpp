e9f5824c2723380ea42d2c2b179d9f223bfc7d5d
Seth Bashford <sbash64@gmail.com>
2019-08-16 11:00:23 -0500

    add test

--- phase-vocoder/tests/OverlapAdd2Tests.cpp
+++ phase-vocoder/tests/OverlapAdd2Tests.cpp
@@ -8,10 +8,10 @@ class OverlapAdd2 {
     std::vector<T> next_;
     int hop;
 public:
-    OverlapAdd2(int N, int hop) : overlap_(N), next_(N), hop{hop} {}
+    OverlapAdd2(int N, int hop) : overlap_(N), next_(hop), hop{hop} {}
 
     void add(gsl::span<const T> x) {
-        std::copy(x.begin(), x.end(), next_.begin());
+        std::copy(x.rbegin(), x.rbegin() + hop, next_.rbegin());
         auto begin_ = overlap_.begin() + hop;
         std::transform(
             begin_,
@@ -22,8 +22,11 @@ public:
         );
     }
 
-    void overlap(gsl::span<T> y) {
+    void next(gsl::span<T> y) {
         std::copy(overlap_.begin(), overlap_.end(), y.begin());
+        for (size_t i{0}; i < overlap_.size() - hop; ++i)
+            overlap_.at(i) = overlap_.at(i+hop);
+        std::copy(next_.rbegin(), next_.rend(), overlap_.rbegin());
     }
 };
 }
@@ -35,20 +38,31 @@ namespace {
 constexpr auto N = 5;
 constexpr auto hop = 2;
 class OverlapAdd2Tests : public ::testing::Test {
+    phase_vocoder::OverlapAdd2<double> overlapAdd{N, hop};
 protected:
     void assertOverlap(
         const std::vector<double> &x,
         const std::vector<double> &y
     ) {
-        phase_vocoder::OverlapAdd2<double> overlapAdd(N, hop);
         overlapAdd.add(x);
         std::vector<double> overlap_(N);
-        overlapAdd.overlap(overlap_);
+        overlapAdd.next(overlap_);
         assertEqual(y, overlap_);
+    }
+
+    void consumeAdd(const std::vector<double> &x) {
+        overlapAdd.add(x);
+        std::vector<double> overlap_(N);
+        overlapAdd.next(overlap_);
     }
 };
 
 TEST_F(OverlapAdd2Tests, firstBlockAddedToZeros) {
     assertOverlap({ 1, 2, 3, 4, 5 }, { 0, 0, 1, 2, 3 });
+}
+
+TEST_F(OverlapAdd2Tests, secondBlockOverlapAddedToFirst) {
+    consumeAdd({ 1, 2, 3, 4, 5 });
+    assertOverlap({ 6, 7, 8, 9, 10 }, { 1, 2, 3+6, 4+7, 5+8 });
 }
 }
