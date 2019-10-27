51c2ace8feea51ef6f73f6633856d7ca13ffffe8
Seth Bashford <sbash64@gmail.com>
2019-10-27 18:33:37 -0500

    move to source

--- include/phase-vocoder/InterpolateFrames.hpp
+++ include/phase-vocoder/InterpolateFrames.hpp
@@ -3,8 +3,6 @@
 
 #include "model.hpp"
 #include "utility.hpp"
-#include <algorithm>
-#include <functional>
 
 namespace phase_vocoder {
 template<typename T>
@@ -22,115 +20,24 @@ class InterpolateFrames {
 	bool first_{true};
 	bool skip_{};
 public:
-	InterpolateFrames(int P, int Q, int N) :
-		previousFrame(sizeNarrow<complex_type<T>>(N)),
-		currentFrame(sizeNarrow<complex_type<T>>(N)),
-		accumulatedPhase(sizeNarrow<T>(N)),
-		phaseAdvance(sizeNarrow<T>(N)),
-		resampledMagnitude(sizeNarrow<T>(N)),
-		numerator{std::min(P, Q)},
-		P{P},
-		Q{Q} {}
-
-	void add(const_complex_signal_type<T> x) {
-		copy(currentFrame, previousFrame);
-		copy(x, currentFrame);
-		transformFrames(
-			phaseAdvance,
-			&InterpolateFrames::phaseDifference
-		);
-		if (P > Q && first_) {
-			accumulatePhase();
-			first_ = false;
-		}
-		hasNext_ = true;
-		accumulatePhaseIfNeeded();
-		updateSkip();
-		checkIfNeedMore();
-	}
-
-	bool hasNext() { return hasNext_; }
-
-	void next(complex_signal_type<T> x) {
-		resampleMagnitude();
-		std::transform(
-			begin(resampledMagnitude),
-			end(resampledMagnitude),
-			begin(accumulatedPhase),
-			begin(x),
-			[](T magnitude, T phase) {
-				return std::polar(magnitude, phase);
-			}
-		);
-		accumulatePhaseIfNeeded();
-		numerator += P;
-		updateSkip();
-		checkIfNeedMore();
-	}
-
+	InterpolateFrames(int P, int Q, int N);
+	void add(const_complex_signal_type<T> x);
+	bool hasNext();
+	void next(complex_signal_type<T> x);
 private:
-	void updateSkip() {
-		skip_ = Q < numerator && numerator < Q + P;
-	}
-
-	void accumulatePhaseIfNeeded() {
-		if (numerator != Q && !skip_)
-			accumulatePhase();
-	}
-
-	void checkIfNeedMore() {
-		if (numerator > Q) {
-			numerator -= Q;
-			hasNext_ = false;
-		}
-	}
-
-	T phaseDifference(const complex_type<T>& a, const complex_type<T>& b) {
-		return phase(b) - phase(a);
-	}
-
-	T phase(const complex_type<T>& x) {
-		return std::arg(x);
-	}
-
-	T magnitude(const complex_type<T> &x) {
-		return std::abs(x);
-	}
-
+	void updateSkip();
+	void accumulatePhaseIfNeeded();
+	void checkIfNeedMore();
+	T phaseDifference(const complex_type<T>& a, const complex_type<T>& b);
+	T phase(const complex_type<T>& x);
+	T magnitude(const complex_type<T> &x);
 	void transformFrames(
 		buffer_type<T> &out,
 		T(InterpolateFrames::*f)(const complex_type<T> &, const complex_type<T> &)
-	) {
-		std::function<T(
-			const complex_type<T> &a,
-			const complex_type<T> &b)
-		> f_ = [&](auto a, auto b) {
-			return (this->*f)(a, b);
-		};
-		phase_vocoder::transform(
-			previousFrame,
-			currentFrame,
-			out,
-			f_
-		);
-	}
-
-	T resampleMagnitude(const complex_type<T>& a, const complex_type<T>& b) {
-		T denominator = Q;
-		auto ratio = numerator / denominator;
-		return magnitude(a) * (1 - ratio) + magnitude(b) * ratio;
-	}
-
-	void resampleMagnitude() {
-		transformFrames(
-			resampledMagnitude,
-			&InterpolateFrames::resampleMagnitude
-		);
-	}
-
-	void accumulatePhase() {
-		addFirstToSecond<T>(phaseAdvance, accumulatedPhase);
-	}
+	);
+	T resampleMagnitude(const complex_type<T>& a, const complex_type<T>& b);
+	void resampleMagnitude();
+	void accumulatePhase();
 };
 }
 
