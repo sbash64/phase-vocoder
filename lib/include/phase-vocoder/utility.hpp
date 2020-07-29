1ac684c8fe33f6f516030648e60f83328c642452
Seth Bashford <sbash64@gmail.com>
2020-07-28 19:17:18 -0500

    geemoney

--- lib/include/phase-vocoder/utility.hpp
+++ lib/include/phase-vocoder/utility.hpp
@@ -114,13 +114,6 @@ void copyFirstToSecond(const_buffer_iterator_type<T> sourceBegin,
 }
 
 template <typename T>
-void copyFirstToSecond(const_buffer_iterator_type<T> sourceBegin,
-    const_buffer_iterator_type<T> sourceEnd,
-    buffer_iterator_type<T> destination) {
-    std::copy(sourceBegin, sourceEnd, destination);
-}
-
-template <typename T>
 void copyFirstToSecond(
     const_signal_type<T> source, signal_type<T> destination) {
     copyFirstToSecond<T>(begin(source), end(source), begin(destination));
