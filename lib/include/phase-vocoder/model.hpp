32aedd18002aae34f23a0af6ce99f57d59595c86
Seth Bashford <sbash64@gmail.com>
2020-07-28 22:23:32 -0500

    same

--- lib/include/phase-vocoder/model.hpp
+++ lib/include/phase-vocoder/model.hpp
@@ -26,13 +26,6 @@ template <typename T> using complex_signal_type = signal_type<complex_type<T>>;
 
 template <typename T>
 using const_complex_signal_type = const_signal_type<complex_type<T>>;
-
-template <typename T>
-using complex_signal_iterator_type = typename complex_signal_type<T>::iterator;
-
-template <typename T>
-using const_complex_signal_iterator_type =
-    typename const_complex_signal_type<T>::iterator;
 }
 
 #endif
