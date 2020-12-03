82ad5caac7e06457484fdaf74f7b61070a7b5fb8
Seth Bashford <seth.bashford@protonmail.com>
2020-12-02 23:40:32 -0600

    explicit instantiation

--- lib/include/sbash64/phase-vocoder/HannWindow.hpp
+++ lib/include/sbash64/phase-vocoder/HannWindow.hpp
@@ -6,6 +6,9 @@
 
 namespace sbash64::phase_vocoder {
 template <typename T> auto hannWindow(index_type N) -> std::vector<T>;
+
+extern template auto hannWindow<double>(index_type N) -> std::vector<double>;
+extern template auto hannWindow<float>(index_type N) -> std::vector<float>;
 }
 
 #endif
